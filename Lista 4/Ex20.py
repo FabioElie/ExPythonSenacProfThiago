31be4e0a3e01b180491ff6d8038fee32d7881930
Fabio Elie <fabioelie@outlook.com>
2026-01-13 08:09:03 -0400

    Exercicio 20

---
+++ Lista 4/Ex20.py
@@ -0,0 +1,17 @@
+quantidade: int = 0
+pares: int = 0
+
+while True:
+    numero = int(input("Digite um número inteiro (0 para encerrar): "))
+
+    if numero == 0:
+        break
+
+
+    quantidade += 1
+
+    if numero % 2 == 0:
+        pares += 1
+
+print(f"Quantidade de números lidos: {quantidade}")
+print(f"Quantidade de números pares: {pares}")
